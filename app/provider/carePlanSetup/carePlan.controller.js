94ca66feea6d7910b884eb302074bbf24ee074a6
venkata <venkata.adari@yahoo.com>
2017-03-08 01:19:01 -0600

    Level 1 graph added

--- app/provider/carePlanSetup/carePlan.controller.js
+++ app/provider/carePlanSetup/carePlan.controller.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 
 /* @ngInject */
-module.exports = function CarePlanSetupController(GraphService) {
+module.exports = function CarePlanSetupController($scope, GraphService) {
     var controller = this;
     controller.networkId = "carePlanNetwork";
 
@@ -12,104 +12,16 @@ module.exports = function CarePlanSetupController(GraphService) {
      */
     function activate() {
 
-        //Get Data
-        //Create Nodes
-        //Create edges
-        draw();
-        GraphService.draw();
-    }
+        //Build Data
+        var graphData = GraphService.buildGraphData(),
+            networkEl = document.getElementById(controller.networkId);
 
-    function draw() {
-        var networkEl = document.getElementById(controller.networkId),
-            nData = networkData(),
-            oData = optionsData();
+        GraphService.gDestroy(controller.context);
 
-        controller.networkGraph = new vis.Network(networkEl, nData, oData);
-    }
+        //Draw Graph
+        controller.context = GraphService.draw(networkEl, graphData);
 
-    function redraw(){
-
-    }
-
-    function networkData() {
-        // create a network
-        return {
-            nodes: nodes(),
-            edges: edges()
-        };
-    }
-
-    function optionsData() {
-        return {
-            groups: {
-                usergroups: {
-                    shape: 'icon',
-                    icon: {
-                        face: 'FontAwesome',
-                        code: '\uf0c0',
-                        size: 50,
-                        color: '#57169a'
-                    }
-                },
-                users: {
-                    shape: 'icon',
-                    icon: {
-                        face: 'FontAwesome',
-                        code: '\uf007',
-                        size: 50,
-                        color: '#aa00ff'
-                    }
-                }
-            }
-        };
-    }
-
-    function nodes() {
-        return [{
-            id: 1,
-            label: 'User 1',
-            group: 'users'
-        }, {
-            id: 2,
-            label: 'User 2',
-            group: 'users'
-        }, {
-            id: 3,
-            label: 'Usergroup 1',
-            group: 'usergroups'
-        }, {
-            id: 4,
-            label: 'Usergroup 2',
-            group: 'usergroups'
-        }, {
-            id: 5,
-            label: 'Organisation 1',
-            shape: 'icon',
-            icon: {
-                face: 'FontAwesome',
-                code: '\uf1ad',
-                size: 50,
-                color: '#f0a30a'
-            }
-        }];
-    }
-
-    function edges() {
-        return [{
-            from: 1,
-            to: 3
-        }, {
-            from: 1,
-            to: 4
-        }, {
-            from: 2,
-            to: 4
-        }, {
-            from: 3,
-            to: 5
-        }, {
-            from: 4,
-            to: 5
-        }];
+        //Add events to on select
+        GraphService.onSelect(controller.context);
     }
 };
--- app/shared/services/graph.service.js
+++ app/shared/services/graph.service.js
@@ -1,7 +1,7 @@
+var _ = require('lodash');
 /* @ngInject */
 module.exports = function GraphService() {
     var graphService = {
-        getContext: getContext,
         draw: draw,
         redraw: reDraw,
         addNode: addNode,
@@ -10,20 +10,40 @@ module.exports = function GraphService() {
         editEdge: editEdge,
         addEdge: addEdge,
         removeEdge: removeEdge,
-        getOptions: getOptions,
         updateOptions: updateOptions,
         setEvent: setEvent,
-
+        buildNode: buildNode,
+        buildEdge: buildEdge,
+        buildNodesEdges: buildNodesEdges,
+        getMockData: getMockData,
+        buildGraphData: buildGraphData,
+        onResize: onResize,
+        gDestroy: gDestroy,
+        onSelect: onSelect
     };
 
     return graphService;
 
-    function getContext() {
-        return vis.Network(networkEl, nData, oData);
+    function buildGraphData() {
+        var serverData = getMockData(),
+        gData = buildNodesEdges(serverData);
+
+        return {
+            nodes: gData.nodes,
+            edges: gData.edges
+        };
     }
 
-    function draw() {
-        console.log("Draw goes here");
+    function draw(networkEl, nData) {
+        var oData = getOptions();
+        return new vis.Network(networkEl, nData, oData);
+    }
+
+    function gDestroy(context) {
+        if (context) {
+            context.destroy();
+            context = null;
+        }
     }
 
     function reDraw() {
@@ -54,15 +74,200 @@ module.exports = function GraphService() {
 
     }
 
-    function getOptions() {
-
-    }
-
     function updateOptions() {
 
     }
 
     function setEvent() {
+
+    }
+
+    function buildNodesEdges(sData) {
+
+        var gData = {nodes: [], edges: []},
+            rootNode = buildNode({
+                id: 1,
+                type: "patient",
+                metaData: {}
+            });
+
+        gData.nodes.push(rootNode);
+
+        var parentId = rootNode.id;
+        _.forEach(sData, function (data, key) {
+            var node, edge;
+
+            node = buildNode({
+                id: ++parentId,
+                type: key,
+                metaData: data
+            });
+
+            //Builds edge FROM node to TO node
+            edge = buildEdge(rootNode.id, parentId, key);
+
+            gData.nodes.push(node);
+            gData.edges.push(edge);
+
+        });
+
+        return gData;
+    }
+
+    function buildNode(data) {
+        return {
+            id: data.id,
+            label: getTypeName(data.type),
+            group: data.type
+        };
+    }
+
+    function getColor (type) {
+        return getOptions().groups[type].icon.color;
+    }
+
+    function getTypeName (type) {
+        return getOptions().groups[type].name;
+    }
+
+    function buildEdge(from, to, key) {
+        var color = getColor(key) || '#3A6073';
+        return {
+            from: from,
+            to: to,
+            color: { color: color }
+        };
+    }
+
+    /**
+     * get graph options
+     * @returns
+     */
+    function getOptions() {
+        return {
+            interaction: {
+                navigationButtons: true,
+                keyboard: true
+            },
+            groups: {
+                patient: {
+                    name: "Patient",
+                    shape: 'icon',
+                    icon: {
+                        face: 'FontAwesome',
+                        code: '\uf2be',
+                        size: 60,
+                        color: '#3A6073'
+                    }
+                },
+                questionnaires: {
+                    name: "Questionnaires",
+                    shape: 'icon',
+                    icon: {
+                        face: 'FontAwesome',
+                        code: '\uf022',
+                        size: 40,
+                        color: '#FF9A40'
+                    }
+                },
+                exercises: {
+                    name: "Exercises",
+                    shape: 'icon',
+                    icon: {
+                        face: 'FontAwesome',
+                        code: '\uf2d6',
+                        size: 40,
+                        color: '#FFC240'
+                    }
+                },
+                medicines: {
+                    name: "Medicines",
+                    shape: 'icon',
+                    icon: {
+                        face: 'FontAwesome',
+                        code: '\uf0fa',
+                        size: 40,
+                        color: '#74D1DA'
+                    }
+                },
+                provider: {
+                    name: "Provider",
+                    shape: 'icon',
+                    icon: {
+                        face: 'FontAwesome',
+                        code: '\uf0f0',
+                        size: 40,
+                        color: '#b76ac4'
+                    }
+                }
+            }
+        };
+    }
+
+    function getMockData() {
+        return {
+            "questionnaires": [{
+                "id": 1,
+                "name": "Survey",
+                "author": "Lisa Foster",
+                "effectiveDate": "4/17/2016",
+                "expiryDate": "7/16/2016",
+                "questions": [{"id": 1, "question": "Donec ut dolor."}, {"id": 2, "question": "Donec dapibus."}]
+            }, {
+                "id": 2,
+                "name": "Health",
+                "author": "Bruce Tucker",
+                "effectiveDate": "10/26/2016",
+                "expiryDate": "9/21/2016",
+                "questions": [{"id": 1, "question": "Nunc rhoncus dui vel sem. Sed sagittis."}]
+            }, {
+                "id": 3,
+                "name": "Family History",
+                "author": "Jane Watkins",
+                "effectiveDate": "8/28/2016",
+                "expiryDate": "11/9/2016",
+                "questions": [{
+                    "id": 1,
+                    "question": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo."
+                }, {
+                    "id": 2,
+                    "question": "Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante."
+                }, {"id": 3, "question": "Morbi a ipsum. Integer a nibh. In quis justo."}]
+            }],
+            "exercises": [{"id": 1, "name": "video2", "duration": "30Mins"}, {
+                "id": 2,
+                "name": "video2",
+                "duration": "2hr"
+            }],
+            "medicines": [{
+                "id": 1,
+                "name": "ALCOHOL",
+                "dosage": "o-1-o",
+                "effectiveDate": "3/14/2016",
+                "endDate": "10/8/2016"
+            }, {
+                "id": 2,
+                "name": "Argentum 5",
+                "dosage": "1-1-1",
+                "effectiveDate": "9/18/2016",
+                "endDate": "1/27/2017"
+            }, {
+                "id": 3,
+                "name": "STARCH, CORN",
+                "dosage": "1-1-1",
+                "effectiveDate": "5/19/2016",
+                "endDate": "7/30/2016"
+            }, {"id": 4, "name": "Adalimumab", "dosage": "1-o-1", "effectiveDate": "9/4/2016", "endDate": "11/11/2016"}]
+        };
+    }
+
+    function onSelect(context) {
+        context.on('select', function(params) {
+            document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;
+        });
+    }
+
+    function onResize() {
 
     }
 
